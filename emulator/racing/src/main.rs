37a7e1461cb7a5dc4a3c68f20dc3367fdd43fdad
Michael Kreuzer <m-git@kreuzers.org>
2026-02-10 20:20:49 +0100

    go kart

--- emulator/racing/src/main.rs
+++ emulator/racing/src/main.rs
@@ -2,10 +2,8 @@ use std::f32::consts::PI;
 
 use avian2d::prelude::{forces::ForcesItem, *};
 use bevy::{
-    app,
     color::palettes::css::{GREEN, RED, WHITE, YELLOW},
     diagnostic::FrameTimeDiagnosticsPlugin,
-    gizmos,
     input::mouse::MouseWheel,
     prelude::*,
 };
@@ -23,6 +21,7 @@ fn main() {
             PhysicsDebugPlugin::default(),
         ))
         .insert_resource(Gravity::ZERO)
+        .insert_resource(Time::<Fixed>::from_duration(std::time::Duration::from_secs_f32(1.0 / 200.0)))
         .add_systems(Startup, (setup, track::setup))
         .add_systems(Startup, set_default_zoom.after(setup))
         .add_systems(FixedUpdate, apply_car_forces)
@@ -30,7 +29,8 @@ fn main() {
         .run();
 }
 
-const WHEEL_BASE: f32 = 3.5;
+const WHEEL_BASE: f32 = 1.18;
+const WHEEL_TRACK: f32 = 0.95;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(PerfUiDefaultEntries::default());
@@ -38,7 +38,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Spawn a camera; we'll set a custom default zoom once in `set_default_zoom`.
     commands.spawn(Camera2d);
 
-    let sprite_scale = Vec3::splat(0.02);
+    let sprite_scale = Vec3::splat(0.008);
     let start_point = track::first_point();
 
     // Spawn the player car with wheels as children
@@ -47,31 +47,31 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             Transform::from_xyz(start_point.x, start_point.y, 1.0),
             Visibility::default(),
             RigidBody::Dynamic,
-            LinearDamping(0.8),
+            LinearDamping(0.1),
             //AngularDamping(0.8),
             Car { steer: 0.0 },
         ))
         .with_children(|parent| {
             parent.spawn((
-                Collider::rectangle(1.8, 5.0),
-                Transform::from_xyz(0.0, 2.1, 0.0),
+                Collider::rectangle(1.25, 2.0),
+                Transform::from_xyz(0.0, 0.66, 0.0),
             ));
 
             parent.spawn((
-                Sprite::from_image(asset_server.load("blue_car_without_wheels.png")),
-                Transform::from_xyz(0.0, 2.05, 0.1).with_scale(sprite_scale),
+                Sprite::from_image(asset_server.load("kart.png")),
+                Transform::from_xyz(0.0, 0.66, 0.1).with_scale(sprite_scale),
             ));
 
             // Front left wheel
             parent
                 .spawn((
-                    Transform::from_xyz(-0.75, 3.5, 0.1),
+                    Transform::from_xyz(-WHEEL_TRACK / 2.0, WHEEL_BASE, 0.1),
                     Visibility::default(),
                     FrontWheel,
                 ))
                 .with_children(|parent| {
                     parent.spawn((
-                        Sprite::from_image(asset_server.load("wheel.png")),
+                        Sprite::from_image(asset_server.load("kart_wheel.png")),
                         Transform::default()
                             .with_scale(sprite_scale)
                             .with_rotation(Quat::from_rotation_z(0.0)),
@@ -80,13 +80,13 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             // Front right wheel
             parent
                 .spawn((
-                    Transform::from_xyz(0.75, 3.5, 0.1),
+                    Transform::from_xyz(WHEEL_TRACK / 2.0, WHEEL_BASE, 0.1),
                     Visibility::default(),
                     FrontWheel,
                 ))
                 .with_children(|parent| {
                     parent.spawn((
-                        Sprite::from_image(asset_server.load("wheel.png")),
+                        Sprite::from_image(asset_server.load("kart_wheel.png")),
                         Transform::default()
                             .with_scale(sprite_scale)
                             .with_rotation(Quat::from_rotation_z(PI)),
@@ -137,20 +137,25 @@ fn apply_car_forces(
             gizmos.arrow_2d(position, position + forward * acceleration * 0.3, WHITE);
         }
 
-        let max_steer = PI / 9.0; // Max steering angle (20 degrees);
+        let max_steer = PI / 6.0; // Max steering angle (30 degrees);
+        let steer_rate = 0.05 * car.steer.abs().max(0.1); // Steering speed
         if keyboard.pressed(KeyCode::KeyA) {
-            car.steer = -max_steer
+            car.steer = (-max_steer).max(car.steer - steer_rate);
         } else if keyboard.pressed(KeyCode::KeyD) {
-            car.steer = max_steer;
+            car.steer = max_steer.min(car.steer + steer_rate);
         } else {
             // Return wheels to center when no input
-            car.steer = 0.0;
+            car.steer = if car.steer > 0.0 {
+                (car.steer - steer_rate).max(0.0)
+            } else {
+                (car.steer + steer_rate).min(0.0)
+            };
         }
 
         // Front left
         apply_wheel_force(
             position,
-            forward * WHEEL_BASE + left * -0.75,
+            forward * WHEEL_BASE + left * -WHEEL_TRACK / 2.0,
             Vec2::from_angle(-car.steer).rotate(forward),
             &mut forces,
             &mut gizmos,
@@ -158,27 +163,15 @@ fn apply_car_forces(
         // Front right
         apply_wheel_force(
             position,
-            forward * WHEEL_BASE + left * 0.75,
+            forward * WHEEL_BASE + left * WHEEL_TRACK / 2.0,
             Vec2::from_angle(-car.steer).rotate(forward),
             &mut forces,
             &mut gizmos,
         );
         // Rear left
-        apply_wheel_force(
-            position,
-            left * -0.75,
-            forward,
-            &mut forces,
-            &mut gizmos,
-        );
+        apply_wheel_force(position, left * -WHEEL_TRACK / 2.0, forward, &mut forces, &mut gizmos);
         // Rear right
-        apply_wheel_force(
-            position,
-            left * 0.75,
-            forward,
-            &mut forces,
-            &mut gizmos,
-        );
+        apply_wheel_force(position, left * WHEEL_TRACK / 2.0, forward, &mut forces, &mut gizmos);
 
         // Update wheel rotation
         for child in children.iter() {
@@ -202,14 +195,15 @@ fn apply_wheel_force(
     gizmos.arrow_2d(wheel_pos, wheel_pos + wheel_left * 0.5, YELLOW);
 
     let o = forces.angular_velocity();
-        let l = forces.linear_velocity();
-        let wow = wheel_pos - car_position;
-        let wheel_velocity = l + Vec2::new(-o * wow.y, o * wow.x);
-        gizmos.arrow_2d(wheel_pos, wheel_pos + wheel_velocity * 0.1, GREEN);
-
+    let l = forces.linear_velocity();
+    let wow = wheel_pos - car_position;
+    let wheel_velocity = l + Vec2::new(-o * wow.y, o * wow.x);
+    gizmos.arrow_2d(wheel_pos, wheel_pos + wheel_velocity * 0.1, GREEN);
 
     if wheel_velocity.length() > 0.1 {
-        let force = -wheel_velocity.normalize().dot(wheel_left) * wheel_left * 10.0_f32.min(wheel_velocity.length()*5.0);
+        let force = -wheel_velocity.normalize().dot(wheel_left)
+            * wheel_left
+            * 10.0_f32.min(wheel_velocity.length() * 5.0);
         gizmos.arrow_2d(wheel_pos, wheel_pos + force, RED);
         forces.apply_linear_acceleration_at_point(force, wheel_pos);
     }
@@ -250,7 +244,7 @@ fn update_camera(
             };
 
             ortho.scale *= 1.0 - zoom_delta;
-            ortho.scale = ortho.scale.clamp(0.01, 5.0);
+            ortho.scale = ortho.scale.clamp(0.001, 10.0);
         }
     }
 
